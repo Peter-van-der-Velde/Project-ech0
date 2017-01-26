ae17c7cd22fe465f867abac6a31118fb41785a6c
zroya <petervandervelde2@gmail.com>
2017-01-26 22:57:48 +0100

    made Speech button a bit bigger

--- Android application/UX_OVERDIVE/TimeScript.cs
+++ Android application/UX_OVERDIVE/TimeScript.cs
@@ -1,14 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Timers;
-
 using Android.App;
 using Android.Content;
-using Android.Icu.Util;
 using Android.OS;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
